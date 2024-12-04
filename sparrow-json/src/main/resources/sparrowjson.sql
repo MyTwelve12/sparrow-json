aa6368eb22f6fc534a690e80595768251be6e831
DousnlHan <hanliangliang@dushu365.com>
2024-12-04 15:17:26 +0800

    渠道组新增

--- sparrow-json/src/main/resources/sparrowjson.sql
+++ sparrow-json/src/main/resources/sparrowjson.sql
@@ -67,6 +67,14 @@ INSERT INTO t_sparrow_backend_config
 (tid, template_alias, code, `type`, `desc`, config, create_id, create_time, update_id, update_time, del_flag)
 VALUES(16, 'delete', 'rulesDB', 1, 'DB转换规则-动态添加更新字段到dto中的查询（需替换流程中的数据源），根据输入字段的key作为条件查询表，得到结果中的key映射添加到dto。 field中多条件间以connect字段做连接符，compare比较符支持=、<、>和in，拼接表达式为：connect + tableField + compare + sourceField的value。 默认取key为tableInsertDTO的对象数据，定制化需求自行更改流程。tableItem：对应的子key name，如无需使用子key则填null。valueAlias：指定添加字段的name，如无需使用对象结构（值的键为boObjKey）则填null。boObjKey：添加到多键值对象时，指定添加的key。', NULL, NULL, '2024-11-08 16:55:51', NULL, '2024-11-08 16:55:51', 1);
 
+INSERT INTO t_sparrow_backend_config
+(tid, template_alias, code, `type`, `desc`, config, create_id, create_time, update_id, update_time, del_flag)
+VALUES(17, 'list', 'resultRule', 1, '对查询的返回结果，批量调用rpc获取结果、填充数据，此规则会与fillFieldByTransObj的规则或其他规则配合使用。每个子对象中的boObjList为调用rpc的参数，entityItem表达式为jsonpath表达式，数据为查询es的结果，ListTransType表示是否将获取到的结果，按集合方法进行转换处理，1为将集合元素转为string 2为将集合元素转为int，distinctType表示是否进行去重，1为去重。最后将rpc获取得到的数据存入以transKey为key的对象中、供后续使用。', '[]', NULL, '2024-11-08 16:55:51', NULL, '2024-11-08 16:55:51', 1);
+INSERT INTO t_sparrow_backend_config
+(tid, template_alias, code, `type`, `desc`, config, create_id, create_time, update_id, update_time, del_flag)
+VALUES(18, 'list', 'fillFieldByTransObj', 1, '批量根据转换对象填充查询的返回结果。每个子对象中，根据transKey、获取transKey对应的对象数据。listFilterRule为批量调用结果的过滤规则，targetFieldKey为表示查询结果中的具体key，targetFieldTransType表示根据targetFieldKey、在循环从查询中获取值后做的转换，1转为string 2转为int；sourceFieldKey为transKey对应数据对象中的key，根据sourceFieldKey过滤得到值，表达式为sourceFieldValue = transKeyValue.filter((item) => item.get(sourceFieldKey) == targetFieldValue)。entityItem为从获取的值sourceFieldValue中获取具体赋值对象，valueAlias为赋值的key，targetDefaultValue为默认值。', '[]', NULL, '2024-11-08 16:55:51', NULL, '2024-11-08 16:55:51', 1);
+
+
 ####菜单表###
 CREATE TABLE `t_cms_menu` (
   `tid` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
@@ -128,3 +136,53 @@ CREATE TABLE `t_cms_role_menu_rlt` (
    KEY `idx_cms_role_del_flag` (`fk_role_tid`,`del_flag`),
    KEY `idx_cms_role_menu_del_flag` (`fk_role_tid`,`fk_menu_tid`,`del_flag`)
 ) ENGINE=InnoDB AUTO_INCREMENT=197202 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='管理后台角色菜单关联表';
+
+####渠道组表###
+CREATE TABLE t_channel_group (
+                                 tid int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+                                 name varchar(255) DEFAULT NULL COMMENT '渠道组名称',
+                                 effect_status int(11) DEFAULT NULL COMMENT '生效状态',
+                                 start_time datetime DEFAULT NULL COMMENT '生效开始时间',
+                                 end_time datetime DEFAULT NULL COMMENT '生效结束时间',
+                                 status int(11) DEFAULT '0' COMMENT '状态',
+                                 create_by varchar(64) DEFAULT NULL COMMENT '创建人',
+                                 create_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                                 update_by varchar(64) DEFAULT NULL COMMENT '更新人',
+                                 update_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                                 del_flag int(11) NOT NULL DEFAULT '1' COMMENT '删除标志',
+                                 PRIMARY KEY (tid)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='渠道组表';
+
+####渠道组关联表###
+CREATE TABLE t_channel_group_relation (
+                                          tid int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+                                          channel_group_id int(11) NOT NULL COMMENT '渠道组ID',
+                                          type int(11) DEFAULT NULL COMMENT '类型',
+                                          channel_id int(11) NOT NULL COMMENT '渠道ID',
+                                          goods_group_id int(11) NOT NULL COMMENT '商品组ID',
+                                          create_by varchar(64) DEFAULT NULL COMMENT '创建人',
+                                          create_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                                          update_by varchar(64) DEFAULT NULL COMMENT '更新人',
+                                          update_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                                          del_flag int(11) NOT NULL DEFAULT '1' COMMENT '删除标志',
+                                          PRIMARY KEY (tid),
+                                          KEY idx_channel_group_id (channel_group_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='渠道组关联表';
+
+
+####商品组表###
+CREATE TABLE t_goods_group (
+                               tid int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+                               name varchar(255) DEFAULT NULL COMMENT '商品组名称',
+                               type int(11) DEFAULT NULL COMMENT '商品组类型',
+                               effect_status int(11) DEFAULT NULL COMMENT '生效状态',
+                               start_time datetime DEFAULT NULL COMMENT '生效开始时间',
+                               end_time datetime DEFAULT NULL COMMENT '生效结束时间',
+                               status int(11) DEFAULT '0' COMMENT '状态',
+                               create_by varchar(64) DEFAULT NULL COMMENT '创建人',
+                               create_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                               update_by varchar(64) DEFAULT NULL COMMENT '更新人',
+                               update_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                               del_flag int(11) NOT NULL DEFAULT '1' COMMENT '删除标志',
+                               PRIMARY KEY (tid)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='商品组表';
